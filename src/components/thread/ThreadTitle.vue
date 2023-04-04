9417272222696e088971d74aa1728c603c0166a2
SamMan56 <111656378+SamMan56@users.noreply.github.com>
2023-04-04 20:02:05 +0100

    add loading... to thread title

--- src/components/thread/ThreadTitle.vue
+++ src/components/thread/ThreadTitle.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { APIResponse, Thread } from '../../types';
+import { APIResponse } from '../../types';
 
 const thread_id = window.location.hash.slice(2).split("/")[1]
 const res = await fetch(`https://europe-west2-df-archive.cloudfunctions.net/getThread?thread_id=${thread_id}`);
 const json: APIResponse = await res.json();
 
 var post = {title: "Thread not found..."};
+document.title = "Loading...";
 if (json.type === "thread" && json.structure === "single") {
     post = 
         {
