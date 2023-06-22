dfb710ee7467ab2f73c5570209320540365f531b
SamMan56 <111656378+SamMan56@users.noreply.github.com>
2023-06-22 17:32:16 +0100

    add all simple categories

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,22 +10,45 @@ export default {
 
     computed: {
         query() {
-            return `https://europe-west2-df-archive.cloudfunctions.net/topUsers?category=posts`;
+            return `https://europe-west2-df-archive.cloudfunctions.net/topUsers?category=${this.category}`;
         }
     },
 
-//     data () {
-//         return {
-//             data: "threads",
-//         }
-//     },
+    data () {
+        return {
+            category: "posts",
+        }
+    },
 }
 </script>
 
 <template>
+    <h1 class="pb-4">Top Users</h1>
     <v-sheet border rounded class>
+        <v-toolbar class="pl-4 pr-4" flat>
+            <v-btn-toggle 
+                v-model='category'
+                color="primary"
+            >
+                <v-btn value="posts">
+                    By Posts
+                </v-btn>
+
+                <v-btn value="threads">
+                    By Threads
+                </v-btn>
+
+                <v-btn value="votes_given">
+                    By Votes Given
+                </v-btn>
+
+                <v-btn value="votes_received">
+                    By Votes Received
+                </v-btn>
+            </v-btn-toggle>
+        </v-toolbar>
         <div class="pa-4">
-            <InfiniteScroll :query="query" infinite/>
+            <InfiniteScroll :query="query" infinite :key="category"/>
         </div>
     </v-sheet>
 </template>
